1c00112ece4204d79a84c68dfe516320a03ec2c6
daor <daor@Lenovo-PC>
2016-12-26 19:00:09 +0800

    修改CRT模式RSA私钥生成工具类

--- RSAKeyJavaPrj/src/cn/z/RSAkey/crtRSAkeyGenerator.java
+++ RSAKeyJavaPrj/src/cn/z/RSAkey/crtRSAkeyGenerator.java
@@ -20,12 +20,19 @@ public class crtRSAkeyGenerator {
 		KeyPair kp = kpg.generateKeyPair();
 		RSAPublicKey publicKey = (RSAPublicKey)kp.getPublic();
 		RSAPrivateCrtKey privateKey = (RSAPrivateCrtKey) kp.getPrivate();
-		System.out.println(ByteUtils.byteArrayToHexString(publicKey.getEncoded()));
-		System.out.println(ByteUtils.byteArrayToHexString(privateKey.getEncoded())); 
+		System.out.println("publicKey:"+ByteUtils.byteArrayToHexString(publicKey.getEncoded()));
+		System.out.println("privateKey:"+ByteUtils.byteArrayToHexString(privateKey.getEncoded())); 
 		
-		System.out.println(ByteUtils.byteArrayToHexString(privateKey.getModulus().toByteArray())); 
-		System.out.println(ByteUtils.byteArrayToHexString(privateKey.getPublicExponent().toByteArray())); 
+		System.out.println("Modulus:"+ByteUtils.byteArrayToHexString(publicKey.getModulus().toByteArray())); 
+		System.out.println("Public Exponent:"+ByteUtils.byteArrayToHexString(publicKey.getPublicExponent().toByteArray())); 
+		System.out.println("Private Exponent:"+ByteUtils.byteArrayToHexString(privateKey.getPrivateExponent().toByteArray())); 
+		System.out.println("Prime P:"+ByteUtils.byteArrayToHexString(privateKey.getPrimeP().toByteArray()).toUpperCase()); 
+		System.out.println("Prime Q:"+ByteUtils.byteArrayToHexString(privateKey.getPrimeQ().toByteArray()).toUpperCase()); 
+		System.out.println("Prime Exponent P:"+ByteUtils.byteArrayToHexString(privateKey.getPrimeExponentP().toByteArray()).toUpperCase()); 
+		System.out.println("Prime Exponent Q:"+ByteUtils.byteArrayToHexString(privateKey.getPrimeExponentQ().toByteArray()).toUpperCase()); 
+		System.out.println("Crt Coefficient:"+ByteUtils.byteArrayToHexString(privateKey.getCrtCoefficient().toByteArray()).toUpperCase()); 
 
+		System.out.println("授权卡RSA私钥CRT分量设置指令：");
 		System.out.println("00B1000040"+ByteUtils.byteArrayToHexString(privateKey.getPrimeP().toByteArray()).toUpperCase()); 
 		System.out.println("00B2000040"+ByteUtils.byteArrayToHexString(privateKey.getPrimeQ().toByteArray()).toUpperCase()); 
 		System.out.println("00B3000040"+ByteUtils.byteArrayToHexString(privateKey.getPrimeExponentP().toByteArray()).toUpperCase()); 
